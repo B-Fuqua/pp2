99665528eed9482bf595bd5a5ca3f94da1fcf0c4
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-04-15 12:47:09 -0400

    added includes file with functions, and removed function from index

--- index.php
+++ index.php
@@ -22,18 +22,11 @@ error_reporting(E_ALL);
 <body>
     <h1>Pair Program 2</h1>
     <?php
+        require "includes/functions.php";
         echo "<h2>PHP Array Practice</h2>";
         $numbers = array(7, 9, 8, 9, 8, 8, 6);
 
         printArr($numbers);
-
-        function printArr($numbers)
-        {
-            foreach ($numbers as $number)
-            {
-                echo "$number <BR>";
-            }
-        }
     ?>
 </body>
 </html>
